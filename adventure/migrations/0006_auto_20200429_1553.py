0187147a019108cec917c1b593d33b4624d5c8d4
Mackenzie Weber <48688377+MWeberLambdaweb19@users.noreply.github.com>
2020-04-29 16:29:38 -0700

    Update 0006_auto_20200429_1553.py

--- adventure/migrations/0006_auto_20200429_1553.py
+++ adventure/migrations/0006_auto_20200429_1553.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('adventure', '0005_auto_20200429_1502'),
+        ('adventure', '0001_initial'),
     ]
 
     operations = [
